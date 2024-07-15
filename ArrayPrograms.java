84f50a8e34b75630f0fa2a699d4cdf9e830f94b8
rasikavaidya24 <134575116+rasikavaidya24@users.noreply.github.com>
2024-07-15 23:08:43 +0530

    Add files via upload

---
+++ ArrayPrograms.java
@@ -0,0 +1,151 @@
+//write a java progam to print elements from 1D array using for loop
+
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		int [] arr={1,2,3,4,5,6};
+// 		int length =0;
+// 		for (int i:arr) {
+// 			length++;
+// 			System.out.println(length);
+			
+// 		}
+// 	}
+// }
+// 0/p=1
+// 2
+// 3
+// 4
+// 5
+// 6
+
+// write a java progam to print elements from 1D array using while loop
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		 int i=1, n=6;
+// 		 while(i<=n){
+// 		 	System.out.println(i);
+// 		 	i++;
+// 		 }
+// 	}
+// }
+
+
+
+// write a java progam to print elements from 1D array using do-while loop
+// class Main {
+//   public static void main(String[] args)
+//   {
+//     int i = 1, n = 6;
+//     do {
+//       System.out.println(i);
+//       i++;
+//     } while(i <= n);
+//   }
+// }
+
+
+//write a java progam to print elements from 1D array using for-each loop
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		int[] arr= {1,2,3,4,5,6};
+
+// 		for(int i:arr)
+// 			System.out.println(i);
+// 	}
+// }
+
+//               ***********2D ARRAY****************
+
+//write a java progam to print elements from 2D array using for loop
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		int [][]arr={{1,2,3},{4,5,6}};
+
+// 		for (int i=0;i<arr.length ;i++ ) {
+// 			for (int j=0;j<arr[i].length ;j++ ) {
+// 				System.out.println(arr[i][j] + " ");
+				
+// 			}
+// 			System.out.println();			
+// 		}
+// 	}
+// }
+// o/p==1
+// 2
+// 3
+
+// 4
+// 5
+// 6
+
+
+//write a java progam to print elements from 2D array using while loop program.
+
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		int [][]arr={{1,2,3},{4,5,6}};
+
+// 		int i=0;
+// 		while(i < arr.length)
+// 		{
+// 			int j=0;
+// 			while(j<arr[i].length){
+
+// 				System.out.println(arr[i][j] +" ");
+// 			    j++;
+// 			}
+// 			System.out.println();
+// 			i++;
+// 		}
+// 	}
+// }
+
+//write a java progam to print elements from 2D array using do-while loop program.
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		int [][]arr={{1,2,3},{4,5,6}};
+
+// 		int i=0;
+// 		do
+// 		{
+// 		int j=0;
+// 			do
+// 			{
+// 				System.out.println(arr[i][j] +" ");
+// 				j++;
+// 			}while(j<arr[i].length);
+
+// 		       System.out.println();
+// 		        i++;
+// 	    }while(i<arr.length);
+
+// 	}
+// }
+
+
+//write a java progam to print elements from 2D array using for-each loop program.
+
+// class ArrayPrograms
+// {
+// 	public static void main(String[] args) {
+// 		int [][]arr={{1,2,3},{4,5,6}};
+
+// 		for (int[]row: arr)         //Loop through each row  
+// 		 {
+// 			for (int element: row)   //Loop through each element in the row
+// 			{
+// 				System.out.println(element + " ");
+				
+// 			}
+// 			System.out.println();			
+// 		}
+// 	}
+// }
+
+*******************************@D
